22a69e71ef4b6eaf01e13488c0e28cef00116699
Qomuna <qomuna2008@gmail.com>
2026-03-07 22:25:52 +0700

    fix

--- prisma/migrations/20260128132530_reset_book_student/migration.sql
+++ prisma/migrations/20260128132530_reset_book_student/migration.sql
@@ -5,10 +5,10 @@
 
 */
 -- DropForeignKey
-ALTER TABLE `book` DROP FOREIGN KEY `Book_studentId_fkey`;
+ALTER TABLE `Book` DROP FOREIGN KEY `Book_studentId_fkey`;
 
 -- DropIndex
-DROP INDEX `Book_studentId_fkey` ON `book`;
+DROP INDEX `Book_studentId_fkey` ON `Book`;
 
 -- AlterTable
-ALTER TABLE `book` DROP COLUMN `studentId`;
+ALTER TABLE `Book` DROP COLUMN `studentId`;
