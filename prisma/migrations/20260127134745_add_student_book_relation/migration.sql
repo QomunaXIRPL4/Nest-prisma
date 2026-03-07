8dc61ebf2a3b58cf49e9968afa331eded54e313c
Qomuna <qomuna2008@gmail.com>
2026-03-07 22:23:06 +0700

    fix railway build

--- prisma/migrations/20260127134745_add_student_book_relation/migration.sql
+++ prisma/migrations/20260127134745_add_student_book_relation/migration.sql
@@ -1,5 +1,5 @@
 -- AlterTable
-ALTER TABLE `book` ADD COLUMN `studentId` INTEGER NULL;
+ALTER TABLE `Book` ADD COLUMN `studentId` INTEGER NULL;
 
 -- AddForeignKey
 ALTER TABLE `Book` ADD CONSTRAINT `Book_studentId_fkey` FOREIGN KEY (`studentId`) REFERENCES `Student`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
